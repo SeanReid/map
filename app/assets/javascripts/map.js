f6a474bd437f68b05ae29bb86ab78ef0d3bd350d
Sean Reid <sreid757@gmail.com>
2014-10-14 10:39:10 -0400

    fixed mapid so map displays correctly

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -32,7 +32,7 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
           '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'examples.map-i86knfo3'
+        id: 'sreid757.jp0h08kl'
       }).addTo(map);
 
 L.marker([37.1255699,-76.5480068]).addTo(map)
